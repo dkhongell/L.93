581e6b6b7404ce774328d82f646c8604c954dbff
dkhongell <116841735+dkhongell@users.noreply.github.com>
2022-10-27 16:29:44 -0400

    Add files via upload

---
+++ L.93.py
@@ -0,0 +1,15 @@
+#Brenna and Kalena
+string='SPAM!HelloSPAM! worldSPAM!!'
+def remove_substring(string, remove,add):
+    output=[]
+    index=0
+    while index < len(string):
+        if string[index:index+ len(remove)]==remove:
+            index += len(remove)
+            output.append(add)
+        else:
+            output.append(string[index])
+            index+=1
+    print("".join(output))
+
+remove_substring("hello world", "hello", "teacher")
